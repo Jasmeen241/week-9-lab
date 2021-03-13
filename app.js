4c4f5dabdbadf0e4f4c03dc7938acfafdf471ba3
Jasmeen241 <jasnoorkaursidhu2002@gmail.com>
2021-03-13 07:46:24 +0530

    adding jp file

---
+++ app.js
@@ -0,0 +1,20 @@
+//creating 2 constants and 2 variables
+const c = 9
+
+var str = "pizza"
+
+const bol = true
+
+var num = 88
+
+// Using if statement
+if (num === c);
+
+console.log("no luck");
+
+//Using for loop to iterate over characters of string
+for (let i = 0; i < str.length; i++){ 
+    console.log(str[i]) 
+  } 
+
+
